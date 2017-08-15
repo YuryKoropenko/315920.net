d63c89e9d283c844f90ba628892ff7412154bc8c
YuryKoropenko <yurykoropenko@gmail.com>
2017-08-15 13:34:19 +0300

    min adaptive(not final)

--- js/main.js
+++ js/main.js
@@ -6,4 +6,14 @@ $(document).ready(function() {
 		$(this).removeClass('l-menu__item-active');
 		$(this).children('.l-menu__sub').stop(true, false).slideUp();
 	});
+
+	$('.mob-menu .red_bg').on('click', function() {
+		$('#header-menu').slideToggle();
+	});
+
+	$('.mob-l-menu a').on('click', function() {
+		$(this).toggleClass('active');
+		$('.l-menu').slideToggle();
+		return false;
+	});
 });
